2b907616cb07e86723bae005e8c08601ccafcbca
LUKAmod1910 <fk4h6108@gmail.com>
2020-10-16 13:01:21 +0900

    データ型・文字列クラス課題
    Task1_1.java
    Task1_1.class の作成

---
+++ JavaStudyのコピー/Task1_1.java
@@ -0,0 +1,22 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+
+      int hogeInt;
+      hogeInt = 50;
+      hogeInt = 100;
+      System.out.println(hogeInt);
+
+
+      String hogeString;
+      hogeString = "テスト";
+      hogeString = "合格";
+       /*変数hogeStringの中身を表示*/
+      System.out.println(hogeString);
+
+  /*double型の hogedouble という変数を宣言、0.2を代入*/
+      double hogeDouble;
+      hogeDouble = 0.2;
+      System.out.println(hogeDouble);
+
+}
+}
