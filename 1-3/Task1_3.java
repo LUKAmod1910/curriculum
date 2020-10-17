59a0179a2486d8534f2245cef11f68f091012dd0
LUKAmod1910 <fk4h6108@gmail.com>
2020-10-17 16:20:00 +0900

    演算子
    Task1_3.class
    Task1_3.java の作成

---
+++ 1-3/Task1_3.java
@@ -0,0 +1,38 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+        // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
+				int plus;
+				plus = 5 + 10;
+
+        // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
+				int minus;
+				minus = 20 - 7;
+
+        // ③下記の処理について、何をしているのかコメントを記入してください。
+        // [10に2かけた値を代入する、変数multiplayの作成]
+        int multiplay = 10 * 2;
+
+        // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
+				int remi;
+				remi = 20 % 6;
+
+        // ⑤下記の処理について、何をしているのかコメントを記入してください
+        // [String型の変数town・lineに文字列有楽町・線代入]
+        String town = "有楽町";
+        String line = "線";
+
+        // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
+				String train;
+				train = town + line;
+
+        // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
+					System.out.println(plus);
+					System.out.println(minus);
+					System.out.println(multiplay);
+					System.out.println(remi);
+					System.out.println(train);
+
+    }
+}
